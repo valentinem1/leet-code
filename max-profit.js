8aa60bd2bc6a88f96126e4c181fefb41c17f3223
Valentine <valentinemaillard1@gmail.com>
2020-04-05 12:48:43 -0400

    max profit solution

---
+++ max-profit.js
@@ -0,0 +1,25 @@
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+// Example 1:
+
+// Input: [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+function maxProfit(prices) {
+    let profit = 0;
+
+    for (let i = 0; i < prices.length - 1; i++) {
+        const diff = prices[i + 1] - prices[i];
+        if (diff > 0) profit += diff;
+    }
+    return profit;
+};
+console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([1,2,3,4,5]))
+console.log(maxProfit([7,6,4,3,1]))
